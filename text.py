50e205e1f44b92cf8908868531b7e36ba5f07bbd
Nishanth Sri Harsha Tirukkovalluri <156707851+nishanth8164@users.noreply.github.com>
2024-01-19 20:58:49 -0600

    Update text.py

--- text.py
+++ text.py
@@ -1,8 +1,8 @@
-with open('input_file.txt','r') as ipf:#created a file named input_file and used read and split functions to read the file and split the words into several words as per the question
-    line=ipf.read()
-    word=line.split()
+with open('input_file.txt','r') as ipf:#create input file 
+    line=ipf.read()# read input file
+    word=line.split() # split line into words to perform wordcount
     with open('output_file.txt','w') as opf:
         for i in word: # Here i have iterated through word variable where the split of words are returned 
             opf.write(i+':'+str(word.count(i))+'\n')      
 opf=open('output_file.txt','r')
-print(opf.read())
+print(opf.read()) #print wordcount
